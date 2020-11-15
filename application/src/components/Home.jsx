082a652ec3630a9ab4c1de8dfc6c8b6b0f78cc75
Haugaard <mathiashn9@gmail.com>
2020-11-15 15:26:59 +0100

    Home change

--- application/src/components/Home.jsx
+++ application/src/components/Home.jsx
@@ -2,6 +2,10 @@ export default function Home() {
   return (
     <>
       <h2>Welcome!</h2>
+      <p>
+        It was not hard to use the startcode. All I did was clone the repo from
+        Github and open it in my ide and other programs.
+      </p>
     </>
   );
 }
